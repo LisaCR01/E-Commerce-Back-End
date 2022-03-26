546625ff741f40f24daf91fe22c125a1691dd64a
LisaCR01 <lisagunn01@gmail.com>
2022-03-26 23:03:41 +0000

    added walthrough video to README and got rid of other README file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -70,16 +70,15 @@ router.get('/:id', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-router.post('/', async (req, res) => {
-  try {
-    const productData = await {Product}.create(req.body);
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-;
-
-
+// router.post('/', async (req, res) => {
+//   try {
+router.post('/',  (req, res) => {
+//     const productData = await {Product}.create(req.body);
+//     res.status(200).json(productData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// ;
 Product.create(req.body)
         .then((product) => {
             // if there's product tags, we need to create pairings to bulk create in the ProductTag model
